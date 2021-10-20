20d457ce345369c2e633b77edd194f5b6c6dfe7a
Meplos <alexxd64150@gmail.com>
2021-10-20 21:30:05 +0200

    heroku: config setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ repositories {
     mavenCentral()
 }
 
+task("stage") {
+    dependsOn("installDist")
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
